13399cbf62fb3a8687a00c8fab688dcde513337e
Mat Taylor <mat.taylor@gmail.com>
2018-02-16 13:57:26 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-export.emo = { 
-  username: 'myEmotivUsername'
-  password: 'myEmotivPassword',
-  license : 'myEmotivLicense' 
-  client_id:"dLtw8yCxBXdIqdlDKHmhB2Sx5gI91amybjUGnaJx",
-  client_secret:"Bg84uRfKi4k58tYxkUS0fGpEvrANaYrvQ6CwNwXyRnvWtUTxagerHUW47LxWdWvSlSlGlt6cMdrGL8Pi6iSQwYXrpcTxXE2x0BHDTsYbZjn6iYVqF0Mu0XptfEY5zgjo",
-}
-
-export.osc: {
-  port : 5721,
-  host: 'localhost',
-  protocol: 'udp',
-  streams : ['cog', 'pow', 'eeg']
+module.exports = { 
+  emo: { 
+    username: 'myEmotivUsername',
+    password: 'myEmotivPassword',
+    license : 'myEmotivLicense',
+    client_id:'myClientId',
+    client_secret:'myClientSecret'
+  },
+  osc: {
+    port : 5721,
+    host: 'localhost',
+    protocol: 'udp',
+    streams : ['cog', 'pow', 'eeg']
+  }
 }
 
